ef8511334734fff9d53fd47372da104822d7528b
Tom Hall <tfhall@protonmail.ch>
2020-02-06 12:20:47 +0000

    Updates

---
+++ precache-manifest.224a5a2d025de6671062d34ba2b87af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9478cd15414df16bede11f4af7abbeb",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5079f1117b33f5f1c145",
+    "url": "/monsters/static/css/main.a3c11f59.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5079f1117b33f5f1c145",
+    "url": "/monsters/static/js/main.dbbd1c3a.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  }
+]);
